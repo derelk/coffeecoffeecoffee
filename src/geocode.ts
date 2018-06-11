8ae81248e1148d6d43e65ce12e48783e2abdab9f
Derek Shockey <derek.shockey@gmail.com>
2018-06-10 20:18:22 -0700

    refactor geocode to use custom error class
    Makes it easier to respond appropriately, since the "zero results" error
    should surface as a 400 while others are probably 500.

--- src/geocode.ts
+++ src/geocode.ts
@@ -13,6 +13,18 @@ const geocodeRequest = request.defaults({
     uri: API_URL,
 });
 
+export enum GeocodeResponse { OK, ZERO_RESULTS, OVER_QUERY_LIMIT, REQUEST_DENIED, INVALID_REQUEST, UNKNOWN_ERROR };
+
+export class GeocodeError extends Error {
+    public code: GeocodeResponse;
+
+    constructor(code: GeocodeResponse) {
+        super('Error geocoding address: ' + GeocodeResponse[code]);
+        this.code = code;
+        Object.setPrototypeOf(this, GeocodeError.prototype);
+    }
+}
+
 /**
  * Geocodes a given address string to a promise of ICoordinates. Ignores multiple results and returns only the first.
  * @param {string} address
@@ -22,10 +34,12 @@ export default async function geocode(address: string): Promise<ICoordinates> {
     return new Promise<ICoordinates>((resolve, reject) => {
         geocodeRequest({ qs: { address } })
             .then((response) => {
-                if (response.status === 'OK') {
+                // shenanigans to coerce string to enum
+                const status: GeocodeResponse = (GeocodeResponse as any)[response.status];
+                if (status === GeocodeResponse.OK) {
                     resolve(response.results[0].geometry.location);  // response JSON already conforms to ICoordinates
                 } else {
-                    reject(new Error('Error geocoding address: ' + response.status));
+                    reject(new GeocodeError(status));
                 }
             })
             .catch((err) => {
--- test/geocode.test.ts
+++ test/geocode.test.ts
@@ -1,9 +1,19 @@
-import geocode from '../src/geocode';
+import geocode, { GeocodeError, GeocodeResponse } from '../src/geocode';
 
-test('non-existent address', async () => {
-    await expect(geocode('6151 Squeedlyspooch Lane')).rejects.toThrow('ZERO_RESULTS');
+test('non-existent address', () => {
+    geocode('6151 Squeedlyspooch Lane')
+        .then(() => {
+            fail('Should have thrown GeocodeError');
+        })
+        .catch((err) => {
+            expect(err).toBeInstanceOf(GeocodeError);
+            expect(err.code).toBe(GeocodeResponse.ZERO_RESULTS);
+        });
 });
 
 test('real-ish address', async () => {
-    await expect(geocode('6151 Richmond St')).resolves.toEqual({lat: 37.5934799, lng: -84.4943636});
+    await expect(geocode('6151 Richmond St, Miami, FL')).resolves.toEqual({
+        lat: 25.6087737,
+        lng: -80.49900339999999,
+    });
 });
